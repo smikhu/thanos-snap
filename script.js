fe5a2c39497cabbc2d4c9e82290f86e7682af4ca
Steven Mikhu <StevenMikhu@gmail.com>
2022-05-25 22:50:28 -0400

    index file cleaned up

--- script.js
+++ script.js
@@ -7,26 +7,68 @@ const yellow = document.getElementById('yellow');
 
 orange.addEventListener('click', () => {
     document.body.style.backgroundColor = 'orange'
+    document.body.style.transition = '1s'
+    document.body.style.color = 'black'
+
+    let div = document.getElementById('text');
+    div.className = 'stones'
+    div.innerText = 'SOUL'
+    document.body.appendChild(div)
 })
 
 red.addEventListener('click', () => {
     document.body.style.backgroundColor = 'red'
+    document.body.style.transition = '1s'
+    document.body.style.color = 'white'
+
+    let div = document.getElementById('text');
+    div.className = 'stones'
+    div.innerText = 'REALITY'
+    document.body.appendChild(div)
 })
 
 blue.addEventListener('click', () => {
     document.body.style.backgroundColor = 'lightskyblue'
+    document.body.style.transition = '1s'
+    document.body.style.color = 'black'
+
+    let div = document.getElementById('text');
+    div.className = 'stones'
+    div.innerText = 'SPACE'
+    document.body.appendChild(div)
 })
 
 purple.addEventListener('click', () => {
     document.body.style.backgroundColor = 'violet'
+    document.body.style.transition = '1s'
+    document.body.style.color = 'black'
+
+    let div = document.getElementById('text');
+    div.className = 'stones'
+    div.innerText = 'POWER'
+    document.body.appendChild(div)
 })
 
 green.addEventListener('click', () => {
     document.body.style.backgroundColor = 'green'
+    document.body.style.transition = '1s'
+    document.body.style.color = 'white'
+
+    let div = document.getElementById('text');
+    div.className = 'stones'
+    div.innerText = 'TIME'
+    document.body.appendChild(div)
 })
 
 yellow.addEventListener('click', () => {
     document.body.style.backgroundColor = 'yellow'
+    document.body.style.transition = '1s'
+    document.body.style.color = 'black'
+
+    let div = document.getElementById('text');
+    div.className = 'stones'
+    div.innerText = 'MIND'
+    document.body.appendChild(div)
 })
 
 
